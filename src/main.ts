b95921d5c2c296df74abd88a2baa36359bf527be
Victor Oliveira <victorhugodias2001@gmail.com>
2024-09-02 02:30:14 -0300

    fix(docker-compose): update configuration to use PostgreSQL for Temporal database connection

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
+    .setTitle('Task Management API')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
